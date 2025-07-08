81115fc40b9f0ff63527fef3e26f95db0f5c8dc6
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-07-08 14:48:07 +0500

    Starting layout added, now after clicking button fragments will change

--- app/src/main/java/com/bignerdranch/android/test/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/test/MainActivity.java
@@ -1,23 +1,36 @@
 package com.bignerdranch.android.test;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mChangeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // This layout must have two containers
+        setContentView(R.layout.start_layout); // This layout must have two containers
 
-        if(savedInstanceState==null){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.left, new LeftFragment())
-                    .replace(R.id.right, new RightFragment())
-                    .commit();
+        mChangeButton = (Button) findViewById(R.id.change_button);
+        mChangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+                if(savedInstanceState==null){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.left, new LeftFragment())
+                            .replace(R.id.right, new RightFragment())
+                            .commit();
 
-        }
+                }
+
+            }
+        });
+
+
     }
 }
