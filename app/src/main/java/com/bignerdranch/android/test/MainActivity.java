b87f5b816465546da5fa98980caaa82fa27fc3f6
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-07-09 16:02:15 +0500

    backstack added, when user presses start button fragments will pop-up but when user presses back button it will go to starting page

--- app/src/main/java/com/bignerdranch/android/test/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/test/MainActivity.java
@@ -5,32 +5,29 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     private Button mChangeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_layout); // This layout must have two containers
+        setContentView(R.layout.activity_main); // This layout must have two containers
 
-        mChangeButton = (Button) findViewById(R.id.change_button);
+        mChangeButton = (Button) findViewById(R.id.start_button);
         mChangeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_main);
                 if(savedInstanceState==null){
                     getSupportFragmentManager().beginTransaction()
                             .replace(R.id.left, new LeftFragment())
                             .replace(R.id.right, new RightFragment())
+                            .addToBackStack(null)
                             .commit();
-
                 }
-
             }
         });
-
-
     }
 }
